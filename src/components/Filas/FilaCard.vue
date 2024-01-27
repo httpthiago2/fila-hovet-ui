2c89f5d03d7537e68f67181165f07fdd36dd5610
Cleonor Junior <cleonorjunior09@gmail.com>
2024-01-27 20:05:49 -0300

    feat: Listagem de filas

---
+++ src/components/Filas/FilaCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card bordered class="q-pa-md rounded-borders rounded">
+    <div class="row text-body1 text-weight-bold items-center justify-between">
+        {{ props.cirurgia }}
+      <q-btn
+          icon="fa-solid fa-pen-to-square"
+          unelevated
+      />
+    </div>
+    <div class="row">
+      {{ props.sala }}
+    </div>
+    <div class="row">
+
+    </div>
+    <div class="row justify-end items-center">
+      {{ toggle ? 'Fechada' : 'Aberta' }}
+      <q-toggle
+          v-model="toggle"
+          size="lg"
+          checked-icon="fa-solid fa-lock "
+          :color="toggle ? 'negative' : 'positive'"
+          unchecked-icon="fa-solid fa-lock-open"
+          keep-color
+      />
+    </div>
+
+  </q-card>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+const toggle = ref(false)
+export type FilaCardProps = {
+  cirurgia: string;
+  sala: string;
+  aberta: string;
+}
+
+const props = defineProps<FilaCardProps>()
+</script>
---
+++ src/pages/Filas/ListagemFilas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-ma-md row">
+    <q-card class="full-width">
+      <q-card-section class="bg-primary text-white">
+        <div class="text-h6">Gerenciar Filas</div>
+      </q-card-section>
+
+      <q-card-section class="bg-white q-">
+        <div class="row q-mb-xl">
+          <q-btn color="secondary" label="Nova Fila"/>
+        </div>
+        <div>
+          <div class="row justify-start q-gutter-md" v-if="filas.length > 0">
+            <fila-card class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-12" v-for="fila in filas" :key="fila.id" v-bind="fila"/>
+          </div>
+          <div v-else class="text-h6 text-center"><q-icon name="fa-solid fa-paw"/> Não há filas cadastradas no momento.</div>
+        </div>
+
+
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import FilaCard from 'components/Filas/FilaCard.vue';
+
+const filas = ref([
+    {id: 1, cirurgia: 'Demartologia', sala: 'Sala de Cirurgia1', aberta: 'Aberta'},
+  {id: 2, cirurgia: 'Raio-X', sala: 'Sala 2', aberta: 'Aberta'}
+])
+</script>
