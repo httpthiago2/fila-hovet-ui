5486e37e2fadc6a2cc9d26d28ce652050ed3a32f
Cleonor Junior <cleonorjunior09@gmail.com>
2024-01-28 23:19:56 -0300

    feat: Adição das rotas para crud de fila

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,9 +7,13 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
   {
-    path: '/gerenciar-filas',
+    path: '/fila',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ name: 'GerenciarFilas', path: '', component: () => import('pages/Filas/ListagemFilas.vue') }],
+    children: [
+      { name: 'GerenciarFilas', path: 'index', component: () => import('pages/Filas/ListagemFilas.vue') },
+      { name: 'NovaFila', path: 'create', component: () => import('pages/Filas/FormFila.vue') },
+      { name: 'EditarFila', path: 'edit/:id', component: () => import('pages/Filas/FormFila.vue') },
+    ],
   },
   // Always leave this as last one,
   // but you can also remove it
