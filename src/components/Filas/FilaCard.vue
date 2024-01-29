2c94df2ca21cdcf06c22448bde1eab1184581a27
Cleonor Junior <cleonorjunior09@gmail.com>
2024-01-28 23:22:29 -0300

    refactor: Card de fila

--- src/components/Filas/FilaCard.vue
+++ src/components/Filas/FilaCard.vue
@@ -1,42 +1,49 @@
 <template>
   <q-card bordered class="q-pa-md rounded-borders rounded">
     <div class="row text-body1 text-weight-bold items-center justify-between">
-        {{ props.cirurgia }}
+        {{ props.name }}
       <q-btn
           icon="fa-solid fa-pen-to-square"
           unelevated
+          @click="router.push({ name: 'EditarFila', params: { id: props.id } })"
       />
     </div>
     <div class="row">
-      {{ props.sala }}
+      {{ props.room.name }}
     </div>
     <div class="row">
 
     </div>
-    <div class="row justify-end items-center">
-      {{ toggle ? 'Fechada' : 'Aberta' }}
-      <q-toggle
-          v-model="toggle"
-          size="lg"
-          checked-icon="fa-solid fa-lock "
-          :color="toggle ? 'negative' : 'positive'"
-          unchecked-icon="fa-solid fa-lock-open"
-          keep-color
-      />
+    <div class="row justify-between items-center">
+      <q-badge :color="badge.color" :label="badge.label" />
+      <queue-status-toggle @toggle="onToggle" @init="onToggle" />
     </div>
 
   </q-card>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+import type { Fila } from 'components/models'
+import QueueStatusToggle from 'components/Filas/QueueStatusToggle.vue'
+import { QueueStatus } from 'components/models'
+import { reactive } from 'vue';
 
-import { ref } from 'vue';
-const toggle = ref(false)
-export type FilaCardProps = {
-  cirurgia: string;
-  sala: string;
-  aberta: string;
+
+const router = useRouter()
+
+
+const props = defineProps<Fila>()
+const emit = defineEmits(['block', 'unblock'])
+const badge = reactive({
+  color: '',
+  label: ''
+})
+
+const onToggle = (status: QueueStatus, color: string, label: string) => {
+  status === QueueStatus.OPEN ? emit('unblock') : emit('block')
+  badge.color = color
+  badge.label = label
 }
 
-const props = defineProps<FilaCardProps>()
 </script>
