bee3fb24889945cee4c14b3524a012e4c87dac56
Cleonor Junior <cleonorjunior09@gmail.com>
2024-01-28 23:21:20 -0300

    feat: Mapeamento das classes de Fila

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,3 +6,37 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export enum QueueStatus { OPEN, CLOSED}
+export enum Species { FELINE, CANINE }
+export enum Gender { MALE , FEMALE}
+
+export enum MedicalRecordStatus { ATTENDED, PENDING }
+
+export interface Fila {
+  id?: number;
+  name: string;
+  queueStatus: QueueStatus;
+  room: Room;
+  medicalRecords: MedicalRecord[];
+  userCode: string;
+}
+
+
+export interface Room {
+  id?: number;
+  name: string;
+}
+
+export interface MedicalRecord {
+  id?: string;
+  petName: string;
+  tutor: string;
+  weight: number;
+  registerDate: string;
+  complaint: string;
+  species: Species;
+  gender: Gender ;
+  attendanceOrder: number;
+  medicalRecordStatus: MedicalRecordStatus;
+}
