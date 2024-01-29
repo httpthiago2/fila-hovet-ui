df42575abacda3fac1a3a1bc9e21b03191b74604
Cleonor Junior <cleonorjunior09@gmail.com>
2024-01-28 23:30:12 -0300

    feat: Busca das filas da api

--- src/pages/Filas/ListagemFilas.vue
+++ src/pages/Filas/ListagemFilas.vue
@@ -1,33 +1,55 @@
 <template>
   <div class="q-ma-md row">
     <q-card class="full-width">
-      <q-card-section class="bg-primary text-white">
+      <q-card-section class="row bg-primary text-white justify-between">
         <div class="text-h6">Gerenciar Filas</div>
+        <q-btn icon="close" unelevated @click="router.back()" />
       </q-card-section>
 
       <q-card-section class="bg-white q-">
         <div class="row q-mb-xl">
-          <q-btn color="secondary" label="Nova Fila"/>
+          <q-btn color="secondary" label="Nova Fila" @click="router.push({ name: 'NovaFila' })"/>
         </div>
         <div>
           <div class="row justify-start q-gutter-md" v-if="filas.length > 0">
-            <fila-card class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-12" v-for="fila in filas" :key="fila.id" v-bind="fila"/>
+            <fila-card class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-12" v-for="fila in filas" :key="fila.id" v-bind="fila" @block="() => console.log(`block ${fila.id}`)" @unblock="() => console.log(`unblock ${fila.id}`)" />
           </div>
           <div v-else class="text-h6 text-center"><q-icon name="fa-solid fa-paw"/> Não há filas cadastradas no momento.</div>
         </div>
-
 
       </q-card-section>
     </q-card>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import FilaCard from 'components/Filas/FilaCard.vue';
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import FilaCard from 'components/Filas/FilaCard.vue'
+import { useRouter } from 'vue-router'
+import { Fila } from 'components/models'
+import { useQuasar } from 'quasar'
+import { useQueueApi } from 'src/services/QueueAPI'
 
-const filas = ref([
-    {id: 1, cirurgia: 'Demartologia', sala: 'Sala de Cirurgia1', aberta: 'Aberta'},
-  {id: 2, cirurgia: 'Raio-X', sala: 'Sala 2', aberta: 'Aberta'}
-])
+const q = useQuasar()
+const router = useRouter()
+
+const filas = ref<Fila[]>([])
+const api = useQueueApi()
+const getFilas = async () => {
+  try {
+    const { data } = await api.findAll()
+    filas.value = data?.data
+  } catch (error: any) {
+    q.notify({
+      color: 'negative',
+      message: error.response.data.message
+    })
+  }
+
+
+}
+
+onMounted(async () => {
+  await getFilas()
+})
 </script>
